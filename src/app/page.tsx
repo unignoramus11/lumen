ce4d2d0518aaf918d57a47827fb41d66ef48a9a5
Mohit Singh <unignoramus@gmail.com>
2025-07-01 22:59:26 +0530

    Embrace linting

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,7 @@
 import Image from "next/image";
 import Link from "next/link";
 import { useRouter } from "next/navigation";
-import { useEffect, useState, useRef } from "react";
+import { useEffect, useState } from "react";
 import BanterLoader from "../components/BanterLoader";
 import NewspaperDatePicker from "../components/NewspaperDatePicker";
 import PhotoContent from "../components/PhotoContent";
